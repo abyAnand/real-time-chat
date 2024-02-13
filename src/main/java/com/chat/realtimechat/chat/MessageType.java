a4a6cd9f371aeab7958aa4d2b647fa7db1ac6802
abyAnand <abianand382@gmail.com>
2024-02-13 23:29:20 +0530

    Initial commit

---
+++ src/main/java/com/chat/realtimechat/chat/MessageType.java
@@ -0,0 +1,14 @@
+package com.chat.realtimechat.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
